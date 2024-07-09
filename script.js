083f90d9552fceb8aaf4bae68b2a4f2bef2efbf0
Mohd Amir <140543590+Amir0364@users.noreply.github.com>
2024-07-09 13:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+function addListItem() {
+	
+var list = document.getElementById('infi-list');
+var listItem = document.createElement('li');
+listItem.textContent = 'New Item';
+list.appendChild(listItem);
+}
+for(var i = 0; i< 10; i++){
+	addListItem();
+}
+var list = document.getElementById('infi-list');
+list.addEventListener('scroll', function(){
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight)
+	{
+		addListItem();
+		addListItem();
+	}
+})
 
